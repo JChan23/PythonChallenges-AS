ab4132ddc8c561b1858e611343a0ad4ecc275635
JChan23 <143363241+JChan23@users.noreply.github.com>
2023-10-24 21:17:46 +0800

    Create AS - Mirror text.py

---
+++ AS - Mirror text.py
@@ -0,0 +1,5 @@
+#A jokes shop company wants people to enter in text, but when they display it they want it to be spelt backwards.	
+
+text = input('Enter text for reversal: ')
+reversed = text[::-1]
+print(reversed)
